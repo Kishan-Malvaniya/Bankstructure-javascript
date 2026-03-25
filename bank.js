ed4ba4a931eb92bb547c1a02bbe6ebcebd8fa5bc
Kishan Malvaniya <malvaniyakishan53@gmail.com>
2026-03-25 13:16:22 +0530

    Add files via upload

---
+++ bank.js
@@ -0,0 +1,110 @@
+class bank{
+    constructor(amount){
+        this.amount = amount
+    }
+    deposite(amount) {
+        this.amount = this.amount + amount
+    }
+    withdrow(amount) {
+        this.amount = this.amount - amount
+    }
+    getBalance() {
+        console.log("Balance is" + this.amount)
+    }
+}
+
+var myobj = new bank(1000)
+console.log(myobj.amount)
+myobj.deposite(8000)
+myobj.getBalance()
+myobj.withdrow(5000)
+myobj.getBalance()
+
+
+class bank{
+    constructor(amount){
+        this.amount = amount
+    }
+    deposite(amount){
+        this.amount = this.amount + amount
+    }
+    withdrow(amount){
+        this.amount = this.amount - amount
+    }
+    getBalance(){
+        console.log("Balance is" + this.amount)
+    }
+}
+
+var myobj = new bank(1000)
+console.log(myobj.amount)
+myobj.deposite(8000)
+myobj.getBalance()
+myobj.withdrow(5000)
+myobj.getBalance()
+
+class bank{
+    constructor(amount) {
+        this.amount = amount
+    }
+    deposite(amount){
+        this.amount = this.amount + amount
+    }
+    withdrow(amount){
+        this.amount = this.amount - amount
+    }
+    getBalance(){
+        console.log("Balance is" + this.amount)
+    }
+}
+
+var myobj = new bank(1000)
+console.log(myobj.amount)
+myobj.deposite(8000)
+myobj.getBalance()
+myobj.withdrow(5000)
+myobj.getBalance()
+
+class bank{
+    constructor(amount){
+        this.amount = amount
+    }
+    deposite(amount){
+        this.amount = this.amount + amount
+    }
+    withdrow(amount){
+        this.amount = this.amount - amount
+    }
+    getBalance(){
+        console.log("Balance is" + this.amount)
+    }
+}
+
+var myobj = new bank(1000)
+console.log(myobj.amount)
+myobj.deposite(8000)
+myobj.getBalance()
+myobj.withdrow(5000)
+myobj.getBalance()
+
+class bank{
+    constructor(amount){
+        this.amount = amount
+    }
+    deposite(amount){
+        this.amount = this.amount + amount
+    }
+    withdrow(amount) {
+        this.amount = this.amount -  amount
+    }
+    getBalance() {
+        console.log("Balance is" + this.amount)
+    }
+}
+
+var myobj = new bank(1000)
+console.log(myobj.amount)
+myobj.deposite(8000)
+myobj.getBalance()
+myobj.withdrow(5000)
+myobj.getBalance()
